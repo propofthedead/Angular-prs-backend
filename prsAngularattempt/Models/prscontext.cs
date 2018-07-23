a7ce9df0bafd08ecd182a2e5c2f9d1e697e7fd5d
Foster Stule <camodude5@yahoo.com>
2018-07-22 23:11:17 -0400

    db context

---
+++ prsAngularattempt/Models/prscontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace prsAngularattempt.Models
+{
+	public class prscontext : DbContext
+	{
+
+		public prscontext() : base() { }
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<Vendor> Vendors { get; set; }
+		public DbSet<Product> Products { get; set; }
+
+	}
+}
