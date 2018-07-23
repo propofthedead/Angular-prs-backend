e148d2fde8fac7e25c516dfc3980b1f12a0b32b0
Foster Stule <camodude5@yahoo.com>
2018-07-22 23:11:33 -0400

    added json response class as untility

---
+++ prsAngularattempt/Utility/JsonResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prsAngularattempt.Utility
+{
+	public class JsonResponse
+	{
+		public string Message { get; set; } = "Sucess";
+		public string Result { get; set; } = "ok";
+		public object Data { get; set; }
+		public object Error { get; set; }
+	}
+}
