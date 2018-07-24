40509fb058b393354121479afba694cfc1e48f2a
Foster Stule <camodude5@yahoo.com>
2018-07-24 00:12:01 -0400

    added a new router name

--- prsAngularattempt/App_Start/WebApiConfig.cs
+++ prsAngularattempt/App_Start/WebApiConfig.cs
@@ -13,7 +13,11 @@ namespace prsAngularattempt
 
 			// Web API routes
 			config.MapHttpAttributeRoutes();
-
+			config.Routes.MapHttpRoute(
+				name: "RegularApi",
+				routeTemplate: "{controller}/{action}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
 			config.Routes.MapHttpRoute(
 				name: "DefaultApi",
 				routeTemplate: "api/{controller}/{id}",
